087002b5ba608d9b70a2882cafc1b2af3cc80449
耿小泰 <clawpo@foxmail.com>
2016-11-16 15:20:07 +0800

    Revert "项目的整理"
    This reverts commit 53a68d24afd3d867019f5fe453677acc45d6fe51.

--- AS/ShunJianMei_Hairdresser/shunJianMei_Hairdresser/src/main/java/cc/ruit/shunjianmei/home/manager/material/ProductInstance.java
+++
@@ -1,6 +0,0 @@
-package cc.ruit.shunjianmei.home.manager.material;
-
-public class ProductInstance {
-	public String MaterialID = "";
-	public String Num = "";
-}
--- myAS/shunjianmei/shunJianMei/src/main/java/cc/ruit/shunjianmei/util/MapUtils.java
+++
@@ -1,7 +0,0 @@
-package cc.ruit.shunjianmei.util;
-
-public class MapUtils {
-	public void bd_encrypt(double gg_lat, double gg_lon, double bd_lat,
-			double bd_lon) {
-	}
-}
